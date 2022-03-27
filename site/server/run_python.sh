6ae528e5e35407ed528f997f111c1062862e8eb7
Harshit Gupta <harshit.11235@gmail.com>
2022-03-27 22:04:19 +0530

    change sh

--- site/server/run_python.sh
+++ site/server/run_python.sh
@@ -4,7 +4,7 @@
 sudo rfcomm bind rfcomm1 98:D3:21:F7:7D:45
 
 # vital script
-python -m python_model.NayanCom_vital
+python -m python_model.NayanCom_vital &
 
 # cv script
-# python -m python_model.NayanCom_cv --shape-predictor python_model/models/landmark.dat &
+python -m python_model.NayanCom_cv --shape-predictor python_model/models/landmark.dat &
